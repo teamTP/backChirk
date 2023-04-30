c5aaf4f08ffec949f66ebf34ae2d5d896725f851
Daria <grib.dawa@gmail.com>
2023-04-30 19:08:21 +0300

    timedChirk

--- chirk/src/main/java/ru/vsu/cs/chirk/service/ChirkService.java
+++ chirk/src/main/java/ru/vsu/cs/chirk/service/ChirkService.java
@@ -2,6 +2,7 @@ package ru.vsu.cs.chirk.service;
 
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
@@ -11,6 +12,8 @@ import ru.vsu.cs.chirk.entity.Chirk;
 import ru.vsu.cs.chirk.repository.ChirkRepository;
 import ru.vsu.cs.chirk.repository.UserRepository;
 
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.NoSuchElementException;
@@ -18,21 +21,18 @@ import java.util.NoSuchElementException;
 @Slf4j
 @Component
 @Transactional
+@RequiredArgsConstructor
 public class ChirkService {
-
-    @Autowired
-    private TaskScheduler scheduler;
-    @Autowired
-    private ChirkRepository chirkRepository;
-    @Autowired
-    private UserRepository userRepository;
+    private final  TaskScheduler scheduler;
+    private final ChirkRepository chirkRepository;
+    private final UserRepository userRepository;
     public void createChirk(Chirk chirk) {
         chirk.setDate(LocalDateTime.now());
         chirk.setVisible(true);
         chirk.setUser(userRepository.findById(chirk.getRoleId()).orElseThrow(() -> new NoSuchElementException("User doesn't exist")));
         chirkRepository.save(chirk);
         if (chirk.isOneDay()){
-            scheduler.schedule(new RunnableTask(chirk.getId(), chirkRepository), new Date(System.currentTimeMillis() + (6*1000)));
+            scheduler.schedule(new RunnableTask(chirk.getId(), chirkRepository), Instant.now().plusSeconds(6));
         }
     }
 }
