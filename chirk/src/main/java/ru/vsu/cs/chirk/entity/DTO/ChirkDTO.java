5e8766ff3482b0d7c719aedc3b7a1e8bab4d1d02
pekanov <pekanov95@gmail.com>
2023-04-30 23:22:29 +0300

    added dto

---
+++ chirk/src/main/java/ru/vsu/cs/chirk/entity/DTO/ChirkDTO.java
@@ -0,0 +1,8 @@
+package ru.vsu.cs.chirk.entity.DTO;
+
+public class ChirkDTO {
+
+    private String text;
+    private String date_publication;
+    private String is_one_day;
+}
---
+++ chirk/src/main/java/ru/vsu/cs/chirk/entity/DTO/UserDTO.java
@@ -0,0 +1,11 @@
+package ru.vsu.cs.chirk.entity.DTO;
+
+public class UserDTO {
+
+    private String firstname;
+    private String lastname;
+    private String username;
+
+
+
+}
