3ec9c2154a08d707e93f6c5b1144f6d3e9df6637
Daria <grib.dawa@gmail.com>
2023-05-16 23:15:01 +0300

    delete chirk

--- chirk/src/main/java/ru/vsu/cs/chirk/controller/ChirkController.java
+++ chirk/src/main/java/ru/vsu/cs/chirk/controller/ChirkController.java
@@ -1,9 +1,11 @@
 package ru.vsu.cs.chirk.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.vsu.cs.chirk.entity.Chirk;
 import ru.vsu.cs.chirk.entity.DTO.requestDTO.RequestChirkDTO;
+import ru.vsu.cs.chirk.entity.DTO.requestDTO.RequestChirkIdDTO;
 import ru.vsu.cs.chirk.service.ChirkService;
 @RestController
 @RequestMapping("/chirks")
@@ -16,13 +18,11 @@ public class ChirkController {
         chirkService.createChirk(requestChirkDTO);
     }
     @DeleteMapping("/delete")
-    public void deleteChirk(@RequestBody Chirk chirk) {
-        chirkService.deleteChirk(chirk.getId());
-        System.out.println(chirk);
+    public void deleteChirk(@RequestBody RequestChirkIdDTO requestChirkIdDTO) {
+        chirkService.deleteChirk(requestChirkIdDTO.getIdChirk());
     }
     @PutMapping("/updateVisible")
-    public void updateVisible(@RequestBody Chirk chirk) {
-        chirkService.updateVisible(chirk.getId());
-        System.out.println(chirk);
+    public void updateVisible(@RequestBody RequestChirkIdDTO requestChirkIdDTO) {
+        chirkService.updateVisible(requestChirkIdDTO.getIdChirk());
     }
 }
---
+++ chirk/src/main/java/ru/vsu/cs/chirk/entity/DTO/requestDTO/RequestChirkIdDTO.java
@@ -0,0 +1,9 @@
+package ru.vsu.cs.chirk.entity.DTO.requestDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@Getter
+@AllArgsConstructor
+public class RequestChirkIdDTO {
+        private long idChirk;
+}
