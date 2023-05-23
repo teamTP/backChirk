4d133e8f2ecd3cac3728e82ce1f9f3a8f532fc09
pekanov <pekanov95@gmail.com>
2023-05-23 07:50:54 +0300

    added update tokens

--- chirk/src/main/java/ru/vsu/cs/chirk/controller/UserController.java
+++ chirk/src/main/java/ru/vsu/cs/chirk/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.vsu.cs.chirk.entity.DTO.JwtTokensDto;
 import ru.vsu.cs.chirk.entity.DTO.UserAuthorisationDTO;
 import ru.vsu.cs.chirk.entity.DTO.UserRegistrationDTO;
+import ru.vsu.cs.chirk.security.JwtTokenProvider;
 import ru.vsu.cs.chirk.service.AuthenticationService;
 import ru.vsu.cs.chirk.service.UserService;
 
@@ -25,6 +26,9 @@ public class UserController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    private JwtTokenProvider jwtTokenProvider;
+
+
     @GetMapping("/hello")
     public String hello() {
         userService.hello();
@@ -33,36 +37,47 @@ public class UserController {
     }
 
     @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<JwtTokensDto> register(@RequestBody UserRegistrationDTO userRegistrationDTO) {
-
         public ResponseEntity<JwtTokensDto> register (@RequestBody UserRegistrationDTO userRegistrationDTO) {
-//        UserRegistrationDTO userRegistrationDTO = new UserRegistrationDTO("Oleg", "Oc", "AdminDu@Good", "000000" );
-
         JwtTokensDto token = authenticationService.registerUser(userRegistrationDTO);
+
 
         for (int i = 0; i < 10; i++) {
             System.out.println("register is OK");
         }
+
+
         return ResponseEntity.ok().body(token);
     }
 
 
 
     @PostMapping(value = "/authorisation", produces = MediaType.APPLICATION_JSON_VALUE)
-    //    public ResponseEntity<JwtTokensDto> authorise(@RequestBody UserAuthorisationDTO userAuthorisationDTO) {
-
     public ResponseEntity<JwtTokensDto> authorise (@RequestBody UserAuthorisationDTO userAuthorisationDTO) {
-//        UserAuthorisationDTO userAuthorisationDTO = new UserAuthorisationDTO("AdminDu@Good", "000000" );
-
         JwtTokensDto token = authenticationService.loginUser(userAuthorisationDTO);
+
+
 
         for (int i = 0; i < 10; i++) {
             System.out.println("authorise is OK");
         }
+
+
+
+        return ResponseEntity.ok().body(token);
+    }
+    @PostMapping(value = "/updateTokens", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<JwtTokensDto> updateTokens(@RequestHeader("Authorization") String authorizationHeader){
+        String refreshToken = extractToken(authorizationHeader);
+        JwtTokensDto token = authenticationService.refreshToken(refreshToken);
         return ResponseEntity.ok().body(token);
     }
 
-
-
+    private String extractToken(String authorizationHeader) {
+// Assuming the Authorization header value is in the format "Bearer <token>"
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7); // Extract the token part after "Bearer "
+        }
+        throw new IllegalArgumentException("Invalid Authorization header");
+    }
 
 }
