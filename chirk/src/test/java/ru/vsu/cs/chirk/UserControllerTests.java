9a308c283ee377191da7277800e74d59cd4693fa
Janat Kubiita <127599659+Babipbe@users.noreply.github.com>
2023-05-27 18:46:53 +0300

    Add test for register in UserController

---
+++ chirk/src/test/java/ru/vsu/cs/chirk/UserControllerTests.java
@@ -0,0 +1,57 @@
+package ru.vsu.cs.chirk;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import ru.vsu.cs.chirk.controller.UserController;
+import ru.vsu.cs.chirk.entity.DTO.JwtTokensDto;
+import ru.vsu.cs.chirk.entity.DTO.UserRegistrationDTO;
+import ru.vsu.cs.chirk.security.JwtTokenProvider;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserControllerTests.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class UserControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserController userController;
+
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private UserRegistrationDTO userRegistrationDTO;
+
+    @BeforeEach
+    public void init(){
+        userRegistrationDTO = new UserRegistrationDTO("janat", "java",
+                "janjav@mail.ru", "MyVerySecurePassword");
+    }
+
+    @Test
+    public void testRegister() throws Exception {
+        given(userController.register(userRegistrationDTO)).willAnswer((invocation -> invocation.getArgument(0)));
+
+        String requestBody = objectMapper.writeValueAsString(userRegistrationDTO);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isCreated());
+    }
+}
