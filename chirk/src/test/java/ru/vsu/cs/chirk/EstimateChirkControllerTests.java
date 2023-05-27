04f133fc8e2a18f1e638328772f044f1ff98c30b
Janat Kubiita <127599659+Babipbe@users.noreply.github.com>
2023-05-27 16:58:37 +0300

    Add test for createEstimate method

---
+++ chirk/src/test/java/ru/vsu/cs/chirk/EstimateChirkControllerTests.java
@@ -0,0 +1,63 @@
+package ru.vsu.cs.chirk;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import ru.vsu.cs.chirk.controller.EstimateChirkController;
+import ru.vsu.cs.chirk.entity.DTO.requestDTO.RequestEstimateDTO;
+import ru.vsu.cs.chirk.security.JwtTokenProvider;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = EstimateChirkControllerTests.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class EstimateChirkControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EstimateChirkController estimateChirkController;
+
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private RequestEstimateDTO requestEstimateDTO;
+
+    @Value("${authorizationHeader}")
+    private String authorizationHeader;
+
+    @BeforeEach
+    public void init() {
+        requestEstimateDTO = new RequestEstimateDTO(1, true);
+    }
+
+    @Test
+    public void testCreateEstimate() throws Exception {
+        doNothing().when(estimateChirkController).createEstimate(authorizationHeader, requestEstimateDTO);
+
+        String requestBody = objectMapper.writeValueAsString(requestEstimateDTO);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/estimate/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", authorizationHeader)
+                        .content(requestBody))
+                .andExpect(status().isCreated());
+
+        verify(estimateChirkController).createEstimate(authorizationHeader, requestEstimateDTO);
+    }
+}
